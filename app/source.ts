83700257e75cc6308010e6233bf2ef5dda32daab
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-05-18 22:41:33 -0300

    feat: add icons

--- app/source.ts
+++ app/source.ts
@@ -1,9 +1,15 @@
 import { map } from '@/.map';
+import { create } from '@/components/ui/icon';
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
+import { icons } from 'lucide-react';
 
 export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/',
   rootDir: 'docs',
   source: createMDXSource(map),
+  icon(icon) {
+    if (icon in icons)
+      return create({ icon: icons[icon as keyof typeof icons] });
+  },
 });
