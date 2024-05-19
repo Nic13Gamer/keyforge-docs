68c3814fab1bbbc3259b79bf3e2066d0a258605d
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-05-18 23:09:29 -0300

    feat: package-install mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,15 @@
-import type { MDXComponents } from 'mdx/types';
+import { Step, Steps } from 'fumadocs-ui/components/steps';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultComponents from 'fumadocs-ui/mdx';
+import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    Tab,
+    Tabs,
+    Steps,
+    Step,
   };
 }
