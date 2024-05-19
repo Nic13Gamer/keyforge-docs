0b6a5575b815daef510d6e0d7b61469e74ae812b
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-05-19 01:12:14 -0300

    feat: add TypeTable component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,6 @@
 import { Step, Steps } from 'fumadocs-ui/components/steps';
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
+import { TypeTable } from 'fumadocs-ui/components/type-table';
 import defaultComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
@@ -11,5 +12,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     Tabs,
     Steps,
     Step,
+    TypeTable,
   };
 }
