7362accb186a69545cd09ed0d44bbf8341279e92
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-02-08 23:55:16 -0300

    docs: metadata & favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,30 @@ import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { RootProvider } from 'fumadocs-ui/provider';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import './global.css';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Keyforge Docs',
+    template: `%s | Keyforge Docs`,
+  },
+  description:
+    'Documentation & API Reference for Keyforge. Simple license management.',
+  creator: 'Keyforge',
+  authors: [{ name: 'Nicholas' }],
+  keywords: [
+    'Keyforge docs',
+    'Keyforge documentation',
+    'Keyforge API reference',
+    'Keyforge Node.js',
+    'keyforge-js',
+    'License management',
+    'Software licensing',
+  ],
+};
 
 const inter = Inter({
   subsets: ['latin'],
