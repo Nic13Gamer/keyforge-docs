f41346a3f82f2cfa4f8faacb9a815ef1905a1c6d
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-03-25 18:52:21 -0300

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import { source } from '@/lib/source';
+import { MetadataRoute } from 'next';
+
+export const revalidate = false;
+
+const baseUrl =
+  process.env.NODE_ENV === 'development' || !process.env.VERCEL_URL
+    ? new URL('http://localhost:3000')
+    : new URL(`https://${process.env.VERCEL_URL}`);
+
+const url = (path: string): string => new URL(path, baseUrl).toString();
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    ...(await Promise.all(
+      source.getPages().map(async (page) => {
+        const priority = page.url === '/' ? 1 : 0.5;
+
+        return {
+          url: url(page.url),
+          changeFrequency: 'weekly',
+          priority,
+        } as MetadataRoute.Sitemap[number];
+      })
+    )),
+  ];
+}
