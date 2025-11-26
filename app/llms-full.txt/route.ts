461fd7781a7427ae386bdfeb6d4a54966d398d8c
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2025-11-26 18:21:51 -0300

    docs: llms

--- app/llms-full.txt/route.ts
+++ app/llms-full.txt/route.ts
@@ -1,9 +1,9 @@
-import { getLLMText, source } from '@/lib/source';
+import { getLlmText, source } from '@/lib/source';
 
 export const revalidate = false;
 
 export async function GET() {
-  const scan = source.getPages().map(getLLMText);
+  const scan = source.getPages().map(getLlmText);
   const scanned = await Promise.all(scan);
 
   return new Response(scanned.join('\n\n'));
--- lib/source.ts
+++ lib/source.ts
@@ -9,10 +9,10 @@ export const source = loader({
   plugins: [openapiPlugin(), lucideIconsPlugin()],
 });
 
-export async function getLLMText(page: InferPageType<typeof source>) {
+export async function getLlmText(page: InferPageType<typeof source>) {
   const processed = await page.data.getText('processed');
 
-  return `# ${page.data.title}
+  return `# ${page.data.title} (${page.url})
 
 ${processed}`;
 }
